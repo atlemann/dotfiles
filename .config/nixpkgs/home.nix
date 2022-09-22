59e48c7183ae1cb7be64feedc52984758f6edc21
Atle Rudshaug <atle.rudshaug@gmail.com>
2022-09-23 00:01:39 +0200

    [home-manager/i3] Add i3 configuration
    - Use i3status-rust instead of default i3status
    - Use rofi instead of dmenu
    - Add flameshot snipping tool and configure shortcut
    - Configure volume controls via pactl
    - Add workspace_auto_back_and_forth

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   # Home Manager needs a bit of information about you and the
@@ -15,6 +15,7 @@
     direnv
     dotnet-sdk
     emacs
+    flameshot
     font-awesome
     htop
     jetbrains.rider
@@ -41,10 +42,48 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  xsession.enable = true;
-  # xsession.windowManager.i3 = {
-  #   enable = true;
-  #   config = {};
-  #   extraConfig = builtins.readFile ~/.config/i3/config-orig;
-  # };
+  xsession.windowManager.i3 = {
+    enable = true;
+    config = rec {
+      modifier = "Mod4";
+      keybindings = lib.mkOptionDefault {
+        "Print" = "exec \"flameshot gui\"";
+        "${modifier}+d" = "exec --no-startup-id ${pkgs.rofi}/bin/rofi -show run";
+      };
+      bars = [
+        {
+          fonts = {
+            size = 10.0;
+          };
+
+          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ${~/.config/i3status-rust/config-options.toml}";
+        }
+      ];
+    };
+
+    extraConfig = ''
+        # Keyboard volume control
+        bindsym XF86AudioRaiseVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +10% && $refresh_i3status
+        bindsym XF86AudioLowerVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -10% && $refresh_i3status
+        bindsym XF86AudioMute exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle && $refresh_i3status
+
+        # MRU-ish workspace switching by pressing same number again
+        workspace_auto_back_and_forth yes
+      '';
+  };
+
+  programs.i3status-rust = {
+    enable = true;
+    bars = {
+      options = {
+        icons = "awesome";
+        theme = "gruvbox-dark";
+      };
+    };
+  };
+
+  programs.rofi = {
+    enable = true;
+    theme = "gruvbox-dark";
+  }; 
 }
