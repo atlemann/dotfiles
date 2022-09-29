18b21be9d4a670efcb1acd8893e1f1361978e4e2
Atle Rudshaug <atle.rudshaug@gmail.com>
2022-09-29 23:28:03 +0200

    [home-manager] Set alacritty window opacity to 0.9
    - Requires picom to work

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -241,6 +241,9 @@ in
   programs.alacritty = {
     enable = true;
     settings = {
+      window = {
+        opacity = 0.9;
+      };
       colors = {
         primary = {
           background = black;
