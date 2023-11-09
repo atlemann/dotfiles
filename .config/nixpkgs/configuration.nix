32bfb78084000b522d0364935782164e055dd2ca
Atle Rudshaug <atle.rudshaug@gmail.com>
2023-11-09 15:24:01 +0100

    Add NixOS config
    - And modified home-manager to work with it

---
+++ .config/nixpkgs/configuration.nix
@@ -0,0 +1,258 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, lib, pkgs, ... }:
+
+let
+  sources = import ./nix/sources.nix;
+  #  unstable = import sources.unstable { config.allowUnfree = true; };
+
+  # TODO: Get this to work with Rider
+  # my_dotnet = with pkgs; (dotnetCorePackages.combinePackages (with dotnetCorePackages; [
+  #   sdk_6_0
+  #   sdk_7_0
+  #   runtime_6_0
+  #   runtime_7_0
+  # ]));
+  my_dotnet = pkgs.dotnet-sdk_6;
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      "${sources.home-manager}/nixos/default.nix"
+      ./home.nix
+    ];
+
+  documentation.enable = false;
+
+  # Configure the Nix package manager
+  nixpkgs = {
+    pkgs = import sources.nixos {
+      config = {
+        allowUnfree = true;
+      };
+    };
+  };
+
+  nix = {
+#    package = pkgs.nix_2_3;
+    settings = {
+      trusted-users = [ "aru" ];
+      max-jobs = "auto";
+    };
+  };
+
+  # Bootloader.
+  boot = {
+    loader = {
+      grub = {
+        enable = true;
+        device = "/dev/sda";
+        useOSProber = true;
+      };
+
+#      tmp.cleanOnBoot = true;
+    };
+
+    # This is required for dotnet to run correctly
+    kernel.sysctl."fs.inotify.max_user_instances" = 524288;
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Oslo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Enable docker
+  virtualisation = {
+    docker.enable = true;
+    # virtualbox.host.enable = true;
+    # this is needed to get a bridge with DHCP enabled
+    libvirtd.enable = true;
+  };
+
+  # Enable networking
+  networking = {
+    networkmanager.enable = true;
+    hostName = "aru-ws"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  };
+
+  services = {
+    tailscale.enable = true;
+
+    picom = {
+      enable = true;
+      vSync = true;
+      backend = "xrender"; #"glx";
+    };
+
+    # Configure keymap in X11
+    xserver = {
+      layout = "us";
+      xkbVariant = "";
+    };
+
+    # Enable CUPS to print documents.
+    printing.enable = true;
+
+    # Enable the X11 windowing system.
+    xserver = {
+      enable = true;
+      displayManager = {
+        lightdm.enable = true;
+        setupCommands = ''
+          LEFT='DP-6'
+          RIGHT='DP-4'
+          ${pkgs.xorg.xrandr}/bin/xrandr --output $LEFT --primary --auto --output $RIGHT --auto --right-of $LEFT --rotate left
+        '';
+      };
+      windowManager.i3.enable = true;
+      videoDrivers = ["nvidia"];
+    };
+  };
+
+  # Graphics
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.aru = {
+    isNormalUser = true;
+    description = "Atle Rudshaug";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  wget
+    curl
+    my_dotnet
+    emacs
+    emacs-all-the-icons-fonts
+    jetbrains.rider
+
+    # TODO: This doesn't seem to help. The correct SDK's end up in the folder, but Rider can't find them anyway
+    # (jetbrains.rider.overrideAttrs
+    #   (_: {
+    #     postPatch = lib.optionalString (!stdenv.isDarwin) (''
+    #     interp="$(cat $NIX_CC/nix-support/dynamic-linker)"
+    #     patchelf --set-interpreter $interp \
+    #       lib/ReSharperHost/linux-x64/Rider.Backend \
+    #       plugins/dotCommon/DotFiles/linux-x64/JetBrains.Profiler.PdbServer
+
+    #     rm -rf lib/ReSharperHost/linux-x64/dotnet
+    #     ln -s ${my_dotnet} lib/ReSharperHost/linux-x64/dotnet
+    #   '');
+    #   }))
+    pqrs
+    pulseaudio
+    slack
+    spotify
+    #    vmware-horizon-client
+  ];
+
+  environment = {
+    variables = {
+      #     MONITOR_PRIMARY = "eDP-1";
+      DOTNET_ROOT = my_dotnet;
+      # DOTNET_ROOT = with pkgs.dotnetCorePackages; combinePackages ([
+      #   sdk_6_0
+      #   sdk_7_0
+      # ]);
+#      EDITOR = "emacs";
+    };
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+
+  # Security
+  #security.pam.services.gdm.enableGnomeKeyring = true;
+  #services.gnome.gnome-keyring.enable = true;
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  fonts = {
+    fontconfig = {
+      enable = true;
+      antialias = true;
+    };
+    fonts = with pkgs; [
+      fira-code
+      font-awesome
+      jetbrains-mono
+    ];
+  };
+
+  fileSystems."/mnt/internal" = {
+    device = "jackson.resoptima.local:/mnt/bigstorage/internal";
+    fsType = "nfs";
+    options = ["x-systemd.automount" "noauto"];
+  };
+
+  fileSystems."/mnt/external" = {
+    device = "jackson.resoptima.local:/mnt/bigstorage/external";
+    fsType = "nfs";
+    options = ["x-systemd.automount" "noauto"];
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -1,9 +1,8 @@
 { config, lib, pkgs, ... }:
 
 let
-  niv = import ./nix/sources.nix;
-  unstable = import niv.unstable { config.allowUnfree = true; };
-  nixGL = import niv.nixGL {};
+  #  niv = import ./nix/sources.nix;
+  #  unstable = import niv.unstable { config.allowUnfree = true; };
 
   # gruvbox-dark colors
 
@@ -32,343 +31,304 @@ let
 
 in
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "aru";
-  home.homeDirectory = "/home/aru";
+  # There two properties are important to align home-manager with
+  # global nixpkgs set.
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = false;
 
-  nixpkgs.config = {
-    allowUnfree = true;
-  };
+  home-manager.users.aru = {
+    home.stateVersion = config.system.stateVersion; #"23.05";
 
-  home.packages = with pkgs; [
-    curl
-    dotnet-sdk
-    emacs-all-the-icons-fonts
-    fira-code
-    font-awesome
-    unstable.github-copilot-intellij-agent
-    jetbrains-mono # Font
-    jetbrains.rider
+    # Home Manager needs a bit of information about you and the
+    # paths it should manage.
+    home.username = "aru";
+    home.homeDirectory = "/home/aru";
 
-    # TODO: make a flake for nix-channel --add https://github.com/guibou/nixGL/archive/main.tar.gz nixgl && nix-channel --update
-    nixGL.auto.nixGLDefault
+    #  home.sessionVariables = {
+    #    NIX_PATH="${config.home.homeDirectory}/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels"; #${NIX_PATH:+:$NIX_PATH}";
+    #    DOTNET_ROOT = "$(dirname $(realpath $(which dotnet)))";
+    #    SSH_AUTH_SOCK = "\${SSH_AUTH_SOCK:-$XDG_RUNTIME_DIR/ssh-agent.socket}";
+    #    LD_LIBRARY_PATH = "\$LD_LIBRARY_PATH:${lib.strings.makeLibraryPath [ pkgs.fontconfig pkgs.xorg.libICE pkgs.xorg.libSM pkgs.xorg.libX11 ]}";
+    #  };
 
-    pqrs
+    # Let Home Manager install and manage itself.
+    programs.home-manager.enable = true;
 
-    slack
-    spotify
+    # #  services.emacs = {
+    # #    enable = true;
+    # #    client.enable = true;
+    # #    defaultEditor = true;
+    # #  };
+    # #
+    # #  programs.emacs.enable = true;
 
-    tree
+    programs.bash.enable = true;
 
-    vmware-horizon-client
-
-    # Required for Avalonia
-    fontconfig
-    xorg.libICE
-    xorg.libSM
-    xorg.libX11
-  ];
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "23.05";
-
-  home.sessionVariables = {
-    NIX_PATH="${config.home.homeDirectory}/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels"; #${NIX_PATH:+:$NIX_PATH}";
-    DOTNET_ROOT = "$(dirname $(realpath $(which dotnet)))";
-    SSH_AUTH_SOCK = "\${SSH_AUTH_SOCK:-$XDG_RUNTIME_DIR/ssh-agent.socket}";
-    LD_LIBRARY_PATH = "\$LD_LIBRARY_PATH:${lib.strings.makeLibraryPath [ pkgs.fontconfig pkgs.xorg.libICE pkgs.xorg.libSM pkgs.xorg.libX11 ]}";
-  };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  fonts.fontconfig.enable = true;
-
-  services.emacs = {
-    enable = true;
-    client.enable = true;
-    defaultEditor = true;
-  };
-
-  programs.emacs.enable = true;
-  
-  services.dunst = {
-    enable = true;
-  };
-
-  services.flameshot = {
-    enable = true;
-    settings = {
-      General = {
-        startupLaunch = true;
-        showDesktopNotification = true;
-        disabledTrayIcon = false;
-        showStartupLaunchMessage = false;
-      };
+    programs.direnv = {
+      enable = true;
+      enableBashIntegration = true;
     };
-  };
 
-  services.picom = {
-    enable = true;
-    vSync = true;
-    backend = "xrender";
-  };
+    services.dunst = {
+      enable = true;
+    };
 
-  xsession = {
-    enable = true;
-  };
-
-  xsession.windowManager.i3 = {
-    enable = true;
-    config = rec {
-      menu = "${pkgs.rofi}/bin/rofi -show";
-      modifier = "Mod4";
-      keybindings = lib.mkOptionDefault {
-        "Print" = "exec \"flameshot gui\"";
-        "${modifier}+Return" = "exec nixGL alacritty";
-        "${modifier}+q" = "split toggle";
-        "${modifier}+h" = "focus left";
-        "${modifier}+j" = "focus down";
-        "${modifier}+k" = "focus up";
-        "${modifier}+l" = "focus right";
-      };
-      workspaceAutoBackAndForth = true;
-      bars = [
-        {
-          fonts = {
-            names = [ "Jetbrains Mono" ];
-            size = 9.0;
-          };
-
-          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ${~/.config/i3status-rust/config-options.toml}";
-
-          # Gruvbox-dark: https://github.com/jmattheis/i3wm-gruvbox-dark
-          colors = {
-            background = black;
-            statusline = lightgray;
-            focusedWorkspace = {
-              background = lightgray;
-              border = lightgray;
-              text = black;
-            };
-            inactiveWorkspace =  {
-              background = darkgray;
-              border = darkgray;
-              text = lightgray;
-            };
-            activeWorkspace = {
-              background = darkgray;
-              border = darkgray;
-              text = lightgray;
-            };
-            urgentWorkspace = {
-              background = red;
-              border = red;
-              text = black;
-            };
-          };
-        }
-      ];
-      colors = {
-        background = black;
-        focused = {
-          background = lightgray;
-          border = lightgray;
-          childBorder = darkgray;
-          indicator = magenta;
-          text = black;
-        };
-        focusedInactive = {
-          background = darkgray;
-          border = darkgray;
-          childBorder = darkgray;
-          indicator = magenta;
-          text = lightgray;
-        };
-        # placeholder = {
-        #   background = "#0c0c0c";
-        #   border = "#000000";
-        #   childBorder = "#0c0c0c";
-        #   indicator = "#000000";
-        #   text = "#ffffff";
-        # };
-        unfocused = {
-          background = darkgray;
-          border = darkgray;
-          childBorder = darkgray;
-          indicator = magenta;
-          text = lightgray;
-        };
-        urgent = {
-          background = red;
-          border = red;
-          childBorder = red;
-          indicator = red;
-          text = white;
+    services.flameshot = {
+      enable = true;
+      settings = {
+        General = {
+          startupLaunch = true;
+          showDesktopNotification = true;
+          disabledTrayIcon = false;
+          showStartupLaunchMessage = false;
         };
       };
-      startup = [
-        { command = "systemctl --user import-environment XAUTHORITY DISPLAY"; notification = false; } # Required for picom to start
-        { command = "systemctl --user restart picom.service"; notification = false; } # The line above seems not to be enough, so adding this as well
-        { command = "${pkgs.feh}/bin/feh --no-fehbg --bg-fill '${config.home.homeDirectory}/.dotfiles/wallpapers/wp11058332-gruvbox-wallpapers.png'"; notification = false; }
-      ];
     };
-    extraConfig = ''
-        # Keyboard volume control
-        bindsym XF86AudioRaiseVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +10% && $refresh_i3status
-        bindsym XF86AudioLowerVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -10% && $refresh_i3status
-        bindsym XF86AudioMute exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle && $refresh_i3status
 
-        # Stick worspaces to specific screens
-        workspace 1 output DVI-I-2
-        workspace 2 output DVI-I-2
-        workspace 3 output DVI-I-2
-        workspace 4 output DVI-I-2
-        workspace 5 output DVI-I-2
-        workspace 6 output DVI-I-3
-        workspace 7 output DVI-I-3
-        workspace 8 output DVI-I-3
-        workspace 9 output DVI-I-3
-        workspace 10 output DVI-I-3
+    # xsession = {
+    #   enable = true;
+    # };
 
-        # Assign apps to workspaces
-        assign [class="Vivaldi-stable"] 4
-        assign [class="Google-chrome"] 4
-        assign [class="Slack"] 5
-        assign [class="Code"] 6
-        assign [class="jetbrains-rider"] 7
-        assign [class="Vmware-view"] 8
-        for_window [class="Spotify"] move to workspace 9
-      '';
-  };
+    xsession.windowManager.i3 = {
+      enable = true;
+      config = rec {
+        menu = "${pkgs.rofi}/bin/rofi -show";
+        modifier = "Mod4";
+        keybindings = lib.mkOptionDefault {
+          "Print" = "exec \"flameshot gui\"";
+          "${modifier}+Return" = "exec alacritty";
+          "${modifier}+q" = "split toggle";
+          "${modifier}+h" = "focus left";
+          "${modifier}+j" = "focus down";
+          "${modifier}+k" = "focus up";
+          "${modifier}+l" = "focus right";
+        };
+        workspaceAutoBackAndForth = true;
+        bars = [
+          {
+            fonts = {
+              names = [ "Jetbrains Mono" ];
+              size = 9.0;
+            };
 
-  programs.i3status-rust = {
-    enable = true;
-    bars = {
-      options = {
-        icons = "awesome6";
-        theme = "gruvbox-dark";
-      };
-    };
-  };
+            statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-options.toml";
 
-  programs.rofi = {
-    enable = true;
-    theme = "gruvbox-dark";
-    extraConfig = {
-      modi = "combi";
-      show-icons = true;
-      hide-scrollbar = true;
-    };
-  };
-
-  programs.feh.enable = true;
-
-  programs.chromium = {
-    enable = true;
-    package = pkgs.vivaldi;
-    extensions = [
-      { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
-      { id = "gfbliohnnapiefjpjlpjnehglfpaknnc"; } # Surfingkeys
-    ];
-  };
-
-  programs.firefox = {
-    enable = true;
-  };
-
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      window = {
-        opacity = 0.9;
-      };
-      font = {
-        normal.family = "Jetbrains Mono";
-        size = 10;
-      };
-      colors = {
-        primary = {
+            # Gruvbox-dark: https://github.com/jmattheis/i3wm-gruvbox-dark
+            colors = {
+              background = black;
+              statusline = lightgray;
+              focusedWorkspace = {
+                background = lightgray;
+                border = lightgray;
+                text = black;
+              };
+              inactiveWorkspace =  {
+                background = darkgray;
+                border = darkgray;
+                text = lightgray;
+              };
+              activeWorkspace = {
+                background = darkgray;
+                border = darkgray;
+                text = lightgray;
+              };
+              urgentWorkspace = {
+                background = red;
+                border = red;
+                text = black;
+              };
+            };
+          }
+        ];
+        colors = {
           background = black;
-          foreground = white;
+          focused = {
+            background = lightgray;
+            border = lightgray;
+            childBorder = darkgray;
+            indicator = magenta;
+            text = black;
+          };
+          focusedInactive = {
+            background = darkgray;
+            border = darkgray;
+            childBorder = darkgray;
+            indicator = magenta;
+            text = lightgray;
+          };
+          # placeholder = {
+          #   background = "#0c0c0c";
+          #   border = "#000000";
+          #   childBorder = "#0c0c0c";
+          #   indicator = "#000000";
+          #   text = "#ffffff";
+          # };
+          unfocused = {
+            background = darkgray;
+            border = darkgray;
+            childBorder = darkgray;
+            indicator = magenta;
+            text = lightgray;
+          };
+          urgent = {
+            background = red;
+            border = red;
+            childBorder = red;
+            indicator = red;
+            text = white;
+          };
         };
-        normal = {
-          black = black;
-          red = red;
-          green = green;
-          yellow = yellow;
-          blue = blue;
-          magenta = magenta;
-          cyan = cyan;
-          white = white;
-        };
-        bright = {
-          black = bright.black;
-          red = bright.red;
-          green = bright.green;
-          yellow = bright.yellow;
-          blue = bright.blue;
-          magenta = bright.magenta;
-          cyan = bright.cyan;
-          white = bright.white;
+        startup = [
+          #        { command = "systemctl --user import-environment XAUTHORITY DISPLAY"; notification = false; } # Required for picom to start
+          #        { command = "systemctl --user restart picom.service"; notification = false; } # The line above seems not to be enough, so adding this as well
+          #        { command = "${pkgs.feh}/bin/feh --no-fehbg --bg-fill '${config.users.users.aru.home}/.dotfiles/wallpapers/wp11058332-gruvbox-wallpapers.png'"; notification = false; }
+        ];
+      };
+      extraConfig = ''
+            # Keyboard volume control
+            bindsym XF86AudioRaiseVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +10% && $refresh_i3status
+            bindsym XF86AudioLowerVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -10% && $refresh_i3status
+            bindsym XF86AudioMute exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle && $refresh_i3status
+
+            # Stick worspaces to specific screens
+            workspace 1 output DP-6
+            workspace 2 output DP-6
+            workspace 3 output DP-6
+            workspace 4 output DP-6
+            workspace 5 output DP-6
+            workspace 6 output DP-6
+            workspace 7 output DP-6
+            workspace 8 output DP-4
+            workspace 9 output DP-4
+            workspace 10 output DP-4
+
+            # Assign apps to workspaces
+            assign [class="jetbrains-rider"] 3
+            assign [class="Code"] 4
+            assign [class="Vmware-view"] 7
+
+            assign [class="Slack"] 8
+            assign [class="Vivaldi-stable"] 9
+            assign [class="Google-chrome"] 9
+            for_window [class="Spotify"] move to workspace 10
+          '';
+    };
+
+    programs.i3status-rust = {
+      enable = true;
+      bars = {
+        options = {
+          icons = "awesome6";
+          theme = "gruvbox-dark";
         };
       };
     };
-  };
 
-  programs.vscode = {
-    enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      eamodio.gitlens
-      humao.rest-client
-      ionide.ionide-fsharp
-      jdinhlife.gruvbox
-      mhutchie.git-graph
-      ms-dotnettools.csharp
-      ms-vscode-remote.remote-ssh
-    ];
-    userSettings = {
-      "workbench.colorTheme" = "Gruvbox Dark Medium";
-      "editor.fontLigatures" = true;
-      "editor.fontFamily" = "Jetbrains Mono";
-      "editor.fontSize" = 14;
-      "notebook.lineNumbers" = "on";
-      "files.autoSave" = "afterDelay";
-      "FSharp.inlayHints.typeAnnotations" = false;
-      "FSharp.inlayHints.parameterNames" = false;
-      "FSharp.inlayHints.enabled" = false;
+    programs.rofi = {
+      enable = true;
+      theme = "gruvbox-dark";
+      extraConfig = {
+        modi = "combi";
+        show-icons = true;
+        hide-scrollbar = true;
+      };
     };
-  };
 
-  programs.git = {
-    enable = true;
-    userName = "Atle Rudshaug";
-    userEmail = "atle.rudshaug@gmail.com";
-    aliases = {
-      co = "checkout";
-      cob = "checkout -b";
-      ci = "commit";
-      st = "status";
-      br = "branch";
-      bra = "branch -a";
-      hist = "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short";
-      ec = "config --global -e";
+    programs.feh.enable = true;
+
+    programs.chromium = {
+      enable = true;
+      package = pkgs.vivaldi;
+      extensions = [
+        #      { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
+        #      { id = "gfbliohnnapiefjpjlpjnehglfpaknnc"; } # Surfingkeys
+      ];
     };
-  };
 
-  programs.direnv = {
-    enable = true;
-    enableBashIntegration = true;
-  };
+    programs.firefox = {
+      enable = true;
+    };
 
-  programs.bottom.enable = true;
-  programs.htop.enable = true;
+    programs.alacritty = {
+      enable = true;
+      settings = {
+        window = {
+          opacity = 0.9;
+        };
+        font = {
+          normal.family = "Jetbrains Mono";
+          size = 10;
+        };
+        colors = {
+          primary = {
+            background = black;
+            foreground = white;
+          };
+          normal = {
+            black = black;
+            red = red;
+            green = green;
+            yellow = yellow;
+            blue = blue;
+            magenta = magenta;
+            cyan = cyan;
+            white = white;
+          };
+          bright = {
+            black = bright.black;
+            red = bright.red;
+            green = bright.green;
+            yellow = bright.yellow;
+            blue = bright.blue;
+            magenta = bright.magenta;
+            cyan = bright.cyan;
+            white = bright.white;
+          };
+        };
+      };
+    };
+
+    programs.vscode = {
+      enable = true;
+      extensions = with pkgs.vscode-extensions; [
+        eamodio.gitlens
+        humao.rest-client
+        ionide.ionide-fsharp
+        jdinhlife.gruvbox
+        mhutchie.git-graph
+        ms-dotnettools.csharp
+        ms-vscode-remote.remote-ssh
+      ];
+      userSettings = {
+        "workbench.colorTheme" = "Gruvbox Dark Medium";
+        "editor.fontLigatures" = true;
+        "editor.fontFamily" = "Jetbrains Mono";
+        "editor.fontSize" = 14;
+        "notebook.lineNumbers" = "on";
+        "files.autoSave" = "afterDelay";
+        "FSharp.inlayHints.typeAnnotations" = false;
+        "FSharp.inlayHints.parameterNames" = false;
+        "FSharp.inlayHints.enabled" = false;
+      };
+    };
+
+    programs.git = {
+      enable = true;
+      userName = "Atle Rudshaug";
+      userEmail = "atle.rudshaug@gmail.com";
+      aliases = {
+        co = "checkout";
+        cob = "checkout -b";
+        ci = "commit";
+        st = "status";
+        br = "branch";
+        bra = "branch -a";
+        hist = "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short";
+        ec = "config --global -e";
+      };
+    };
+
+    programs.bottom.enable = true;
+    programs.htop.enable = true;
+  };
 }
