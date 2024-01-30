05b68f7fcb37270f80e91156c2d1a927833f3b18
Atle Rudshaug <atle.rudshaug@gmail.com>
2024-01-30 13:02:13 +0100

    [niv] Bump sources to 23.11
    - Also bump unstable channel

--- .config/nixpkgs/configuration.nix
+++ .config/nixpkgs/configuration.nix
@@ -170,9 +170,6 @@ in
     ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
