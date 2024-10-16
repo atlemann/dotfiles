79c1975d774abde780a96e90bfb26a9846b7d161
Atle Rudshaug <atle.rudshaug@gmail.com>
2024-10-16 22:05:00 +0200

    [nix] Move home-manager desktop stuff
    - Add a desktop specific home-manager file

--- .config/nixpkgs/home-common.nix
+++ .config/nixpkgs/home-common.nix
@@ -1,34 +1,7 @@
 { config, lib, pkgs, ... }:
 
 let
-  #  niv = import ./nix/sources.nix;
-  #  unstable = import niv.unstable { config.allowUnfree = true; };
-
-  # gruvbox-dark colors
-
-  # https://github.com/jmattheis/i3wm-gruvbox-dark
-  darkgray = "#1d2021";
-  lightgray = "#bdae93";
-
-  # https://github.com/eendroroy/alacritty-theme/blob/master/themes/gruvbox_dark.yaml
-  black =  "#282828";
-  red =    "#cc241d";
-  green =  "#98971a";
-  yellow = "#d79921";
-  blue =   "#458588";
-  magenta ="#b16286";
-  cyan =   "#689d6a";
-  white =  "#a89984";
-
-  bright.black =  "#928374";
-  bright.red =    "#fb4934";
-  bright.green =  "#b8bb26";
-  bright.yellow = "#fabd2f";
-  bright.blue =   "#83a598";
-  bright.magenta ="#d3869b";
-  bright.cyan =   "#8ec07c";
-  bright.white =  "#ebdbb2";
-
+  theme = import ./theme.nix;
 in
 {
   # There two properties are important to align home-manager with
@@ -39,30 +12,15 @@ in
   home-manager.backupFileExtension = "backup";
 
   home-manager.users.aru = {
-    home.stateVersion = config.system.stateVersion; #"23.05";
+    home.stateVersion = config.system.stateVersion;
 
     # Home Manager needs a bit of information about you and the
     # paths it should manage.
     home.username = "aru";
     home.homeDirectory = "/home/aru";
 
-    #  home.sessionVariables = {
-    #    NIX_PATH="${config.home.homeDirectory}/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels"; #${NIX_PATH:+:$NIX_PATH}";
-    #    DOTNET_ROOT = "$(dirname $(realpath $(which dotnet)))";
-    #    SSH_AUTH_SOCK = "\${SSH_AUTH_SOCK:-$XDG_RUNTIME_DIR/ssh-agent.socket}";
-    #    LD_LIBRARY_PATH = "\$LD_LIBRARY_PATH:${lib.strings.makeLibraryPath [ pkgs.fontconfig pkgs.xorg.libICE pkgs.xorg.libSM pkgs.xorg.libX11 ]}";
-    #  };
-
     # Let Home Manager install and manage itself.
     programs.home-manager.enable = true;
-
-    # #  services.emacs = {
-    # #    enable = true;
-    # #    client.enable = true;
-    # #    defaultEditor = true;
-    # #  };
-    # #
-    # #  programs.emacs.enable = true;
 
     programs.bash = {
       enable = true;
@@ -81,177 +39,10 @@ in
       enableBashIntegration = true;
     };
 
-    services.dunst = {
-      enable = true;
-    };
-
-    services.flameshot = {
-      enable = true;
-      settings = {
-        General = {
-          startupLaunch = true;
-          showDesktopNotification = true;
-          disabledTrayIcon = false;
-          showStartupLaunchMessage = false;
-        };
-      };
-    };
-
-    # xsession = {
-    #   enable = true;
-    # };
-
-    xsession.windowManager.i3 = {
-      enable = true;
-      config = rec {
-        menu = "${pkgs.rofi}/bin/rofi -show";
-        modifier = "Mod4";
-        keybindings = lib.mkOptionDefault {
-          "Print" = "exec \"flameshot gui\"";
-          "${modifier}+Return" = "exec alacritty";
-          "${modifier}+q" = "split toggle";
-          "${modifier}+h" = "focus left";
-          "${modifier}+j" = "focus down";
-          "${modifier}+k" = "focus up";
-          "${modifier}+l" = "focus right";
-        };
-        workspaceAutoBackAndForth = true;
-        bars = [
-          {
-            fonts = {
-              names = [ "Jetbrains Mono" ];
-              size = 9.0;
-            };
-
-            statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-options.toml";
-
-            # Gruvbox-dark: https://github.com/jmattheis/i3wm-gruvbox-dark
-            colors = {
-              background = black;
-              statusline = lightgray;
-              focusedWorkspace = {
-                background = lightgray;
-                border = lightgray;
-                text = black;
-              };
-              inactiveWorkspace =  {
-                background = darkgray;
-                border = darkgray;
-                text = lightgray;
-              };
-              activeWorkspace = {
-                background = darkgray;
-                border = darkgray;
-                text = lightgray;
-              };
-              urgentWorkspace = {
-                background = red;
-                border = red;
-                text = black;
-              };
-            };
-          }
-        ];
-        colors = {
-          background = black;
-          focused = {
-            background = lightgray;
-            border = lightgray;
-            childBorder = darkgray;
-            indicator = magenta;
-            text = black;
-          };
-          focusedInactive = {
-            background = darkgray;
-            border = darkgray;
-            childBorder = darkgray;
-            indicator = magenta;
-            text = lightgray;
-          };
-          # placeholder = {
-          #   background = "#0c0c0c";
-          #   border = "#000000";
-          #   childBorder = "#0c0c0c";
-          #   indicator = "#000000";
-          #   text = "#ffffff";
-          # };
-          unfocused = {
-            background = darkgray;
-            border = darkgray;
-            childBorder = darkgray;
-            indicator = magenta;
-            text = lightgray;
-          };
-          urgent = {
-            background = red;
-            border = red;
-            childBorder = red;
-            indicator = red;
-            text = white;
-          };
-        };
-        startup = [
-          #        { command = "systemctl --user import-environment XAUTHORITY DISPLAY"; notification = false; } # Required for picom to start
-          #        { command = "systemctl --user restart picom.service"; notification = false; } # The line above seems not to be enough, so adding this as well
-          #        { command = "${pkgs.feh}/bin/feh --no-fehbg --bg-fill '${config.users.users.aru.home}/.dotfiles/wallpapers/wp11058332-gruvbox-wallpapers.png'"; notification = false; }
-        ];
-      };
-      extraConfig = ''
-            # Keyboard volume control
-            bindsym XF86AudioRaiseVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +10% && $refresh_i3status
-            bindsym XF86AudioLowerVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -10% && $refresh_i3status
-            bindsym XF86AudioMute exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle && $refresh_i3status
-
-            # Stick worspaces to specific screens
-            workspace 1 output DP-6
-            workspace 2 output DP-6
-            workspace 3 output DP-6
-            workspace 4 output DP-6
-            workspace 5 output DP-6
-            workspace 6 output DP-6
-            workspace 7 output DP-6
-            workspace 8 output DP-4
-            workspace 9 output DP-4
-            workspace 10 output DP-4
-
-            # Assign apps to workspaces
-            assign [class="jetbrains-rider"] 3
-            assign [class="Code"] 4
-            assign [class="Vmware-view"] 7
-
-            assign [class="Slack"] 8
-            assign [class="Vivaldi-stable"] 9
-            assign [class="Google-chrome"] 9
-            for_window [class="Spotify"] move to workspace 10
-          '';
-    };
-
-    programs.i3status-rust = {
-      enable = true;
-      bars = {
-        options = {
-          icons = "awesome6";
-          theme = "gruvbox-dark";
-        };
-      };
-    };
-
-    programs.rofi = {
-      enable = true;
-      theme = "gruvbox-dark";
-      extraConfig = {
-        modi = "combi";
-        show-icons = true;
-        hide-scrollbar = true;
-      };
-    };
-
-    programs.feh.enable = true;
-
-    programs.firefox = {
-      enable = true;
-    };
-
+    services.dunst.enable = true;
+    programs.bottom.enable = true;
+    programs.htop.enable = true;
+    
     programs.alacritty = {
       enable = true;
       settings = {
@@ -264,66 +55,30 @@ in
         };
         colors = {
           primary = {
-            background = black;
-            foreground = white;
+            background = theme.black;
+            foreground = theme.white;
           };
           normal = {
-            black = black;
-            red = red;
-            green = green;
-            yellow = yellow;
-            blue = blue;
-            magenta = magenta;
-            cyan = cyan;
-            white = white;
+            black = theme.black;
+            red = theme.red;
+            green = theme.green;
+            yellow = theme.yellow;
+            blue = theme.blue;
+            magenta = theme.magenta;
+            cyan = theme.cyan;
+            white = theme.white;
           };
           bright = {
-            black = bright.black;
-            red = bright.red;
-            green = bright.green;
-            yellow = bright.yellow;
-            blue = bright.blue;
-            magenta = bright.magenta;
-            cyan = bright.cyan;
-            white = bright.white;
+            black = theme.bright.black;
+            red = theme.bright.red;
+            green = theme.bright.green;
+            yellow = theme.bright.yellow;
+            blue = theme.bright.blue;
+            magenta = theme.bright.magenta;
+            cyan = theme.bright.cyan;
+            white = theme.bright.white;
           };
         };
-      };
-    };
-
-    programs.vscode = {
-      enable = true;
-      mutableExtensionsDir = false;
-      extensions = with pkgs.vscode-extensions; [
-        eamodio.gitlens
-        humao.rest-client
-        ionide.ionide-fsharp
-        jdinhlife.gruvbox
-        mhutchie.git-graph
-        ms-dotnettools.csharp
-        ms-dotnettools.vscode-dotnet-runtime
-        ms-vscode-remote.remote-ssh
-      ];
-      userSettings = {
-        "workbench.colorTheme" = "Gruvbox Dark Medium";
-        "notebook.lineNumbers" = "on";
-        "files.autoSave" = "afterDelay";
-
-        # Fonts
-        "editor.fontFamily" = "Jetbrains Mono";
-        "editor.fontSize" = 14;
-        "editor.fontLigatures" = true;
-
-        # Whitespace
-        "editor.renderWhitespace" = "all";
-        "files.trimTrailingWhitespace" = true;
-        "files.trimFinalNewlines" = true;
-        "files.insertFinalNewline" = true;
-
-        # F#
-        "FSharp.inlayHints.typeAnnotations" = false;
-        "FSharp.inlayHints.parameterNames" = false;
-        "FSharp.inlayHints.enabled" = false;
       };
     };
 
@@ -360,8 +115,5 @@ in
         };
       };
     };
-
-    programs.bottom.enable = true;
-    programs.htop.enable = true;
   };
 }
