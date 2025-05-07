30c4e6dd0ee2c9938014b9234779cf64ad6c50f0
Atle Rudshaug <atle.rudshaug@gmail.com>
2025-05-07 13:20:42 +0200

    [aru-ws] Fix i3 config
    - Use new common volume controls
    - Use the config.assigns attrset instead of hand-writing in
      extraConfig.

--- .config/nixpkgs/hosts/aru-ws/home.nix
+++ .config/nixpkgs/hosts/aru-ws/home.nix
@@ -5,15 +5,16 @@ let
   {
     home-manager.users.aru = {
       xsession.windowManager.i3 = {
-        extraConfig = ''
-            # Keyboard volume control
-            bindsym XF86AudioRaiseVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +10% && $refresh_i3status
-            bindsym XF86AudioLowerVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -10% && $refresh_i3status
-            bindsym XF86AudioMute exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle && $refresh_i3status
-            bindsym $mod+m+up move workspace to output up
-            bindsym $mod+m+down move workspace to output down
-            bindsym $mod+m+right move workspace to output next
+        config.assigns = {
+          "3" = [{ class = "jetbrains-rider"; }];
+          "4" = [{ class = "Code"; }];
+          "6" = [{ class = "Vmware-view"; }];
+          "8" = [{ class = "Slack"; }];
+          "9" = [{ class = "Vivaldi-stable"; } { class = "^Chromium-browser$"; }];
+          "10" = [{ class = "Spotify"; }];
+        };
 
+        extraConfig = ''
             # Stick worspaces to specific screens
             workspace 1 output DP-6
             workspace 2 output DP-6
@@ -25,16 +26,6 @@ let
             workspace 8 output DP-4
             workspace 9 output DP-4
             workspace 10 output DP-4
-
-            # Assign apps to workspaces
-            assign [class="jetbrains-rider"] 3
-            assign [class="Code"] 4
-            assign [class="Vmware-view"] 6
-
-            assign [class="Slack"] 8
-            assign [class="Vivaldi-stable"] 9
-            assign [class="Google-chrome"] 9
-            for_window [class="Spotify"] move to workspace 10
           '';
       };
     };
