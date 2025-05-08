2b84c44419d858b3a8a571443acd4215aea0db1b
Atle Rudshaug <atle.rudshaug@gmail.com>
2025-05-08 21:03:11 +0200

    [woody] Fix i3 config

--- .config/nixpkgs/hosts/woody/home.nix
+++ .config/nixpkgs/hosts/woody/home.nix
@@ -5,12 +5,16 @@ let
   {
     home-manager.users.aru = {
       xsession.windowManager.i3 = {
-        extraConfig = ''
-            # Keyboard volume control
-            bindsym XF86AudioRaiseVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +10% && $refresh_i3status
-            bindsym XF86AudioLowerVolume exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -10% && $refresh_i3status
-            bindsym XF86AudioMute exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle && $refresh_i3status
+        config.assigns = {
+          "3" = [{ class = "Code"; }];
+          "4" = [{ class = "Slack"; }];
+          "5" = [{ class = "Vivaldi-stable"; } { class = "^Chromium-browser$"; }];
+          "6" = [{ class = "jetbrains-rider"; }];
+          "9" = [{ class = "Vmware-view"; }];
+          "10" = [{ class = "Spotify"; }];
+        };
 
+        extraConfig = ''
             # Stick worspaces to specific screens
             workspace 1 output DP-4
             workspace 2 output DP-4
@@ -22,16 +26,6 @@ let
             workspace 8 output DP-0
             workspace 9 output DP-0
             workspace 10 output DP-0
-
-            # Assign apps to workspaces
-            assign [class="jetbrains-rider"] 6
-            assign [class="Code"] 4
-            assign [class="Vmware-view"] 9
-
-            assign [class="Slack"] 4
-            assign [class="Vivaldi-stable"] 5
-            assign [class="Google-chrome"] 5
-            for_window [class="Spotify"] move to workspace 10
           '';
       };
     };
