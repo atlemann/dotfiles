6478eeafaf74fd9794a5a9a8bffed035a305e48c
Atle Rudshaug <atle.rudshaug@gmail.com>
2025-06-17 13:56:32 +0200

    [nix] Explicitly set lightdm as displayManager

--- .config/nixpkgs/modules/wm/i3/default.nix
+++ .config/nixpkgs/modules/wm/i3/default.nix
@@ -20,6 +20,7 @@ in
       (mkIf cfg.enable {
         services.xserver = {
           enable = true;
+          displayManager.lightdm.enable = true;
           windowManager.i3.enable = true;
           xkb = {
             layout = "us";
