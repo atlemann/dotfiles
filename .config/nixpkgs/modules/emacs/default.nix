d27beb0d378748e5e2106062f1d59ed47c89d71a
Atle Rudshaug <atle.rudshaug@gmail.com>
2026-02-06 16:03:23 +0100

    [emacs] Add telegra Telegram client

--- .config/nixpkgs/modules/emacs/default.nix
+++ .config/nixpkgs/modules/emacs/default.nix
@@ -86,6 +86,10 @@ let
     no-littering
     swiper
     tide
+  ] ++ lib.optionals cfg.telegram.enable [
+    # These will only be added to the list if telegram.enable is true
+    telega
+    visual-fill-column
   ]);
 
 in
@@ -97,6 +101,11 @@ in
           default = false;
           description = "Whether to install emacs";
         };
+        telegram.enable = mkOption {
+          type = types.bool;
+          default = false;
+          description = "Whether to enable Telegram support in Emacs";
+        };
       };
     };
 
@@ -106,7 +115,13 @@ in
           my_emacs
           nixd
           nodePackages.typescript-language-server
-        ];
+        ] ++ (lib.optionals cfg.telegram.enable [
+          # System dependencies for Telegram media/stickers
+          tdlib
+          ffmpeg
+          libwebp
+          imagemagick
+        ]);
 
         fonts = {
           packages = with pkgs; [
